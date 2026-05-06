ef8ad361ad0add8eaa1d0b04e20abe82006bbbf4
MANPROCOOL <jose.matias.flores123@gmail.com>
2026-05-05 21:51:15 -0400

    5/05 9 Pm

--- Listado_3/Listado_H.c
+++ Listado_3/Listado_H.c
@@ -1,88 +1,84 @@
 #include <stdio.h>
-#include <string.h>
 
-int lengstr(char comprobar[82]) {
-    int i = 0;
-    while (comprobar[i] != '\n' && comprobar[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
-void imprimir_matriz(int filas, int columnas, char matriz[filas][columnas]) {
-    for (int i = 0; i < filas; i++) {
+void imprimir_matriz(int filas, int columnas, char matriz[101][82]) {
+    for (int i = filas - 1; i >= 0; i--) {
         for (int j = 0; j < columnas; j++) {
             printf("%c", matriz[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-// Devuelve la fila donde está el '*' en la columna col
-int valor_columna(int filas, int columnas, char matriz[filas][columnas], int col) {
-    for (int i = 0; i < filas; i++) {
-        if (matriz[i][col] == '*') return i;
-    }
-    return -1;
-}
+void ordenar(int filas, int columnas, char matriz[101][82]) {
+    int cant[82], min = 0;
 
-// Intercambia dos columnas
-void swap_columnas(int filas, int columnas, char matriz[filas][columnas], int a, int b) {
-    for (int i = 0; i < filas; i++) {
-        char temp = matriz[i][a];
-        matriz[i][a] = matriz[i][b];
-        matriz[i][b] = temp;
-    }
-}
-
-void ordenar_columnas(int filas, int columnas, char matriz[filas][columnas]) {
-    for (int i = 0; i < columnas - 1; i++) {
-        for (int j = 0; j < columnas - 1 - i; j++) {
-            // asterisco más abajo = valor más alto = va a la derecha
-            if (valor_columna(filas, columnas, matriz, j) < valor_columna(filas, columnas, matriz, j + 1)) {
-                swap_columnas(filas, columnas, matriz, j, j + 1);
+    // Calcular altura de cada columna
+    for (int j = 0; j < columnas; j++) {
+        cant[j] = 0;
+        for (int i = 0; i < filas; i++) {
+            if (matriz[filas - 1 - i][j] == '*') {
+                cant[j] = i;
             }
+        }
+    }
+
+    // Reordenar columnas
+    for (int i = 0; i < columnas; i++) {
+        int encontrado = 0;
+        for (int k = 0; k < columnas && !encontrado; k++) {
+            if (cant[k] == min) {
+                encontrado = 1;
+
+                for (int m = 0; m < filas; m++) {
+                    if (m == cant[k]) matriz[m][i] = '*';
+                    else matriz[m][i] = '.';
+                }
+
+                cant[k] = -1; // marcar como usada
+            }
+        }
+
+        if (!encontrado) {
+            min++;
+            i--; // reintentar con el mismo i
         }
     }
 }
 
 int main() {
-    int i = 0, columnas;
-    char lineas[100][82];
-    int primer_log = 1;
+    char lineas[101][82]; 
+    int i = 0, columnas = 0, i_ = 0;
+    char c;
 
-    while (fgets(lineas[i], 82, stdin) != NULL) {
-        if (lineas[i][0] == '\n') {
-            if (i > 0) {
-                columnas = lengstr(lineas[0]);
-                char procesar[i][columnas];
-                for (int r = 0; r < i; r++)
-                    for (int c = 0; c < columnas; c++)
-                        procesar[r][c] = lineas[r][c];
+    while (scanf("%c", &c) != EOF) {
+        if (c == '\r') continue;  // ignorar CR (Windows)
 
-                if (!primer_log) printf("\n");
-                primer_log = 0;
+        lineas[i][i_] = c;
+        i_++;
 
-                ordenar_columnas(i, columnas, procesar);
-                imprimir_matriz(i, columnas, procesar);
+        if (c == '\n') {
+            lineas[i][i_] = '\0';
+
+            if (i_ > 1) {  // línea con contenido
+                columnas = i_ - 1;
+                i++;
+                i_ = 0;
+            } else {  // línea en blanco => procesar bloque
+                if (i > 0) {
+                    ordenar(i, columnas, lineas);
+                    imprimir_matriz(i, columnas, lineas);
+                }
                 i = 0;
+                i_ = 0;
             }
-        } else {
-            i++;
         }
     }
 
-    // Procesar el último log (sin blank line al final)
+    // Procesar último bloque si no termina en línea en blanco
     if (i > 0) {
-        columnas = lengstr(lineas[0]);
-        char procesar[i][columnas];
-        for (int r = 0; r < i; r++)
-            for (int c = 0; c < columnas; c++)
-                procesar[r][c] = lineas[r][c];
-
-        if (!primer_log) printf("\n");
-        ordenar_columnas(i, columnas, procesar);
-        imprimir_matriz(i, columnas, procesar);
+        ordenar(i, columnas, lineas);
+        imprimir_matriz(i, columnas, lineas);
     }
 
     return 0;
